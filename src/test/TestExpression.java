7f4739aef7762fe1ab47773fcdbd622fb40980c0
DeAndreon <ottovanbu@live.it>
2014-11-03 20:03:09 +0100

    Revisione. Apportate modifiche: -codice più snello e rivisto -aggiunta possibilità divisione -divisione restituisce un intero arrotondato -commenti per spiegare la logica
    Sono state testate diverse situazioni come.
    2*(2+1)
    2+1*(1+2)
    e altro

--- src/test/TestExpression.java
+++ src/test/TestExpression.java
@@ -35,78 +35,116 @@ public class TestExpression {
 				     k++;
 			     }
 			     num.push(Integer.parseInt(sbuf.toString()));
-			     System.out.println("num: "+num.top());
+			     //System.out.println("num: "+num.top());
 	    	}else
-		     	if(tokens[i]=='+'||tokens[i]=='-'||tokens[i]=='*'||tokens[i]=='/'){
+		     	if(tokens[i]=='+'||tokens[i]=='-'||tokens[i]=='*'||tokens[i]=='/'||tokens[i]==')'||tokens[i]=='('){
 		     		
 		     		ops.push(tokens[i]);
-		     		if(tokens[i]=='*'||tokens[i]=='/')
-		     		{ 
-		                i++;
-		     			num.push(Integer.valueOf(Character.toString((tokens[i]))));
+		     		//System.out.println(tokens[i]);
+		     		/*quando incontro o una divisione o una moltiplicazione la svolgo e  svuoto anche lo stack
+		     		 * delle operazioni in quanto sicuramente prima(almeno credo) non ci stanno parentesi(perchè
+		     		 * appena incontro una ) svolgo tutto fino ad una ( ma solo
+		     		 * operatori di precedenza minore
+		     		 */
+		     		if(tokens[i]=='*'||tokens[i]=='/'){ 
+		     			if(tokens[i+1] >= '0' && tokens[i+1] <='9'){
+		     				/*prendo il successivo al segno e lo metto in testa allo stack*/
+		     				num.push(Integer.valueOf(Character.toString((tokens[i+1]))));
+		     				
+		     				/* finchè non si svuota lo stack delle operazioni e l'operazione non è una parentesi di chiusura
+		     				 * (perchè altrimenti la sua eliminazione comporta una rottura nell'espressione tra le parentesi)*/
+			     			while(!ops.isEmpty() && ops.top()!='('){
+			     				/*svolgi le operazioni e poi elimina l'operazione dallo stack*/
+			     				doOperation(ops.top(),num);
+			     			    ops.pop();
+			     	        }
+			     		i++;
+		     			}
+		     			
+		     			
 		     			//System.out.println(num.top());
-		     			while(!ops.isEmpty()){
-		     				
-		     				 switch(ops.top()){
-							    
-							    case '*':{
-							    	
-							    	//num.push(num.pop()*Integer.valueOf(Character.toString((tokens[i+1]))));
-							    	num.push(num.pop()*num.pop());
-							    	
-							    	System.out.println("PROVA:" +num.top());
-							    	
-							        break;
-							    }
-							  
-							    case '/':{
-							    	num.push(num.pop()/num.pop());
-							    	System.out.println(num.top());
-							        break;
-							    }
-
-								case '+':{
-									System.out.println(num.top());
-									System.out.println("size: "+num.size());
-									num.push(num.pop()+num.pop());
-								    break;	
-								    	
-								    }
-								  
-								  case '-':{
-									  num.push(num.pop()-num.pop());
-								    	System.out.println(num.top());
-								    	
-								    break;
-								  }
-						}
-		     		    ops.pop();	
-		     				
-		     	    }
-						
-			}
-		}        
-	    
+		     			
+				    	
+		            }  
+		     		
+		     		if(tokens[i]==')'){
+		     			 System.out.println(ops.pop());
+					     while(ops.top()!='('){
+	     				     /*svolgi le operazioni e poi elimina l'operazione dallo stack*/
+	     				     doOperation(ops.top(),num);
+	     				     System.out.println("WEILAAAAAAAAA"+ops.top());
+	     			         ops.pop();
+					
+					     
+				         }
+					System.out.println("WEILAAAAAAAAA"+ops.top());
+				    ops.pop(); /*elimina la '('*/;
+	               }
+		     	}
+	 
 	    if(k!=0){
 		     i=i+k;
 		     k=0;
-		     System.out.println(i);
+		  //   System.out.println("Numero"+i);
 	    }else{
     	     i++;
-    	     System.out.println(i);
+    	    // System.out.println("Segno:"+i);
 	    }
+	   
 	    
-	}   
-	    System.out.println("totale: "+num.pop());
 	}
-
-	
-	
-    private static boolean hasPrecedence(char op1, char op2){
+	while(!ops.isEmpty()) {
+		doOperation(ops.top(),num);
+	    ops.pop();
+		
+	}
+	System.out.println("totale: "+num.pop());	
+		
+		
+}
+    private static void doOperation(char c, MyStack<Integer> num){
+    	int top=0;
     	
-    	if( (op1=='*' || op1=='/' ) && (op2=='+'||op2=='-') )
-    		return true;
-    	else
-    		return false;
+    	
+				
+			 switch(c){
+			    
+			    case '*':{
+			    
+			    	//num.push(num.pop()*Integer.valueOf(Character.toString((tokens[i+1]))));
+			    	num.push(num.pop()*num.pop());
+			    	
+			    	System.out.println("PROVA:" +num.top());
+			    	
+			        break;
+			    }
+			  
+			    case '/':{
+			    	top= num.pop();
+			    	num.push((int)(num.pop()/top));
+			    	System.out.println(num.top());
+			        break;
+			    }
+
+				case '+':{
+					System.out.println(num.top());
+					System.out.println("size: "+num.size());
+					num.push(num.pop()+num.pop());
+				    break;	
+				    	
+				    }
+				  
+				  case '-':{
+					  top= num.pop();
+					  
+					  num.push(num.pop()-top);
+				    	System.out.println(num.top());
+				    	
+				    break;
+				  }
+		}
+		
+    	
+    	
     }
 }
